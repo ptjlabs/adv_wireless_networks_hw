482e9f1c9610edc3226619de8d5787aa327c0181
Preston Turner Jr <preston.turner.jr@gmail.com>
2019-03-02 15:29:36 -0600

    added normalized sigmoid utility function..still buggy

--- assignment_1.py
+++ assignment_1.py
@@ -18,15 +18,38 @@ def norm_log_util_function(rate,rate_of_utilization):
         container.append(math.log(1 + (rate_of_utilization * ri))/(math.log(1 + (rate_of_utilization * max(rate)))))
     return container
 
-user1 = norm_log_util_function(set_rate,15)
-plt.plot(set_rate,user1,label='user1')
+delay_tolerant_user1 = norm_log_util_function(set_rate,15)
+plt.plot(set_rate,delay_tolerant_user1,label='user1')
 
-user2 = norm_log_util_function(set_rate,0.1)
-plt.plot(set_rate,user2,label='user2')
+delay_tolerant_user2 = norm_log_util_function(set_rate,0.1)
+plt.plot(set_rate,delay_tolerant_user2,label='user2')
 
-plt.xlabel('rate')
-plt.ylabel('utilization')
-plt.title('Part A')
+
+'''
+  (b) plot  two  normalized  sigmoid  utility  functions  with a=  5  and b=  10,and a= 0.5 and b= 20.
+      What is the change to the function with increase inthe value of a?  and increase in the value of b?
+
+'''
+
+# To represent real-time applications runningon mobile stations, use the normalized sigmoid utility function
+# def norm_sigmoid_util_function(rate,ai,bi):
+#     sig_container = []
+#     for ri in rate:
+#         # Ci = ((1 + math.exp(ai * bi))/math.exp(ai * bi))
+#         # di = (1/(1 + math.exp(ai * bi)))
+# 	    sig_container.append((((1 + math.exp(ai * bi))/math.exp(ai * bi)) * ((1/1 + math.exp(-ai(ri - bi)) - (1/(1 + math.exp(ai * bi)))))))
+#     return sig_container
+
+# realtime_user1 = norm_sigmoid_util_function(set_rate,5,10)
+# plt.plot(set_rate,realtime_user1,label='realtime-user1')
+
+# realtime_user2 = norm_sigmoid_util_function(set_rate,0.5,20)
+# plt.plot(set_rate,realtime_user2,label='realtime-user2')
+
+
+plt.xlabel('ri')
+plt.ylabel('Ui(ri)')
+plt.title('Assignment 1')
 plt.legend()
 plt.show()
 
