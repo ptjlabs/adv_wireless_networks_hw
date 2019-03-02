35d3249a37cd5340500c70695cd09825abea8a1d
Preston Turner Jr <preston.turner.jr@gmail.com>
2019-03-02 01:50:41 -0600

    graphed first function

---
+++ assignment_1.py
@@ -0,0 +1,30 @@
+# This file will serve as a place to test graphs
+
+
+import numpy as np
+import math
+
+'''
+ (a) Plot two normalized logarithmic utility functions with k = 15
+     and r-max = 50, and k = 0.1 and r-max = 50
+'''
+# Define Rate Set (50 being maximum required rate for the user to achieve 100%
+S_rate = [x for x in range(50)]
+set_rate = np.arange(0.,49.,0.1)
+
+# to represent delay-tolerant applications running on mobilestations, use the normalized logarithmic utility function
+def norm_log_util_function(rate,rate_of_utilization):
+    container = []
+    for ri in rate:
+        container.append(math.log(1 + (rate_of_utilization * ri))/(math.log(1 + (rate_of_utilization * max(rate)))))
+
+    return container
+normalized = norm_log_util_function(set_rate,15)
+print(normalized)
+
+import matplotlib.pyplot as plt
+plt.plot(set_rate,normalized)
+plt.xlabel('rate')
+plt.ylabel('utilization')
+plt.show()
+
--- scratch_paper.py
+++ scratch_paper.py
@@ -2,13 +2,22 @@
 
 import matplotlib.pyplot as plt
 import numpy as np
+import math
 
+# x = np.arange(-8,8,0.1)
+# f = 1 / (1 + np.exp(-x))
 
-x = np.arange(-8,8,0.1)
-f = 1 / (1 + np.exp(-x))
+# plt.plot(x,f)
+# plt.xlabel('x')
+# plt.ylabel('f(x)')
+# plt.show()
 
-plt.plot(x,f)
-plt.xlabel('x')
-plt.ylabel('f(x)')
-plt.show()
+def norm_log_util_function(rate,rate_of_utilization):
+    container = []
+    for ri in rate:
+        math.log(1 + (rate_of_utilization * ri))/(math.log(1 + (rate_of_utilization * max(rate))))
 
+    return container
+
+f = norm_log_util_function([x for x in range(50)],15)
+print(f)
