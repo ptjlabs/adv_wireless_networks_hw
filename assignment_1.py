eda399ed7266faba117d95c1edd4fc3c77d787ea
Preston Turner Jr <preston.turner.jr@gmail.com>
2019-03-03 12:18:51 -0600

    edits

--- assignment_1.py
+++ assignment_1.py
@@ -25,11 +25,11 @@ plt.plot(set_rate,delay_tolerant_user1,label='Log k = 15')
 delay_tolerant_user2 = norm_log_util_function(set_rate,0.1)
 plt.plot(set_rate,delay_tolerant_user2,label='Log k = 0.1')
 
-# plt.xlabel('ri')
-# plt.ylabel('Ui(ri)')
-# plt.title('Assignment 1')
-# plt.legend()
-# plt.show()
+plt.xlabel('ri')
+plt.ylabel('Ui(ri)')
+plt.title('Assignment 1')
+plt.legend()
+plt.show()
 
 #---------------------------------------------------------------------------------------------------------------------------#
 
