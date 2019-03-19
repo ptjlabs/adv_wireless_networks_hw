9acfdddca05589c5b9b390d8be2fd22f5837b522
Preston Turner Jr <preston.turner.jr@gmail.com>
2019-03-18 22:51:23 -0500

    update functions

--- assignment_1.py
+++ assignment_1.py
@@ -8,20 +8,21 @@ from scipy.optimize import curve_fit # https://docs.scipy.org/doc/scipy/referenc
      and r-max = 50, and k = 0.1 and r-max = 50
 '''
 # Define Rate Set (50 being maximum required rate for the user to achieve 100%
-set_rate = np.arange(0.,49.,0.2)
+set_rate = np.arange(0.,47.,0.2)
 
 # to represent delay-tolerant applications running on mobilestations, use the normalized logarithmic utility function
-def norm_log_util_function(rate,rate_of_utilization):
-    container = []
-    for ri in rate:
-        container.append(math.log(1 + (rate_of_utilization * ri))/(math.log(1 + (rate_of_utilization * max(rate)))))
-    return container
+def norm_log_util_function(rate_of_utilization):
+  global set_rate
+  container = []
+  for ri in set_rate:
+      container.append(math.log(1 + (rate_of_utilization * ri))/(math.log(1 + (rate_of_utilization * max(set_rate)))))
+  return container
 
-delay_tolerant_user1 = norm_log_util_function(set_rate,15)
+delay_tolerant_user1 = norm_log_util_function(15)
 plt.plot(set_rate,delay_tolerant_user1,label='Log k = 15')
 
 
-delay_tolerant_user2 = norm_log_util_function(set_rate,0.1)
+delay_tolerant_user2 = norm_log_util_function(0.1)
 plt.plot(set_rate,delay_tolerant_user2,label='Log k = 0.1')
 
 # plt.xlabel('ri')
@@ -38,23 +39,24 @@ plt.plot(set_rate,delay_tolerant_user2,label='Log k = 0.1')
 
 '''
 #To represent real-time applications runningon mobile stations, use the normalized sigmoid utility function
-def norm_sigmoid_util_function(rate,ai,bi):
-    sig_container = []
-    for ri in rate:
-	    sig_container.append( ((1 + math.exp(ai * bi)) / math.exp(ai * bi)) * ((1 / (1 + math.exp(-ai * (ri - bi))) - (1 / (1 + math.exp(ai * bi))))) )
-    return sig_container
+def norm_sigmoid_util_function(ai,bi):
+  global set_rate
+  sig_container = []
+  for ri in set_rate:
+    sig_container.append( ((1 + math.exp(ai * bi)) / math.exp(ai * bi)) * ((1 / (1 + math.exp(-ai * (ri - bi))) - (1 / (1 + math.exp(ai * bi))))) )
+  return sig_container
 
-realtime_user1 = norm_sigmoid_util_function(set_rate,5,10)
+realtime_user1 = norm_sigmoid_util_function(5,10)
 plt.plot(set_rate,realtime_user1,label='Sigmoid, a = 5, b = 10')
 
-realtime_user2 = norm_sigmoid_util_function(set_rate,0.5,20)
+realtime_user2 = norm_sigmoid_util_function(0.5,20)
 plt.plot(set_rate,realtime_user2,label='Sigmoid, a = 0.5, b = 20')
 
-# plt.xlabel('rates')
-# plt.ylabel('Utilization(ri)')
-# plt.title('Utilities')
-# plt.legend()
-# plt.show()
+plt.xlabel('rates')
+plt.ylabel('Utilization(ri)')
+plt.title('Utilities')
+plt.legend()
+plt.show()
 
 #---------------------------------------------------------------------------------------------------------------------------#
 
@@ -65,15 +67,16 @@ eters k and rmax. Plot the functions in (b) and new generated normalized
 logarithmic utility functions in the same figure.
 
 '''
-fit = curve_fit(norm_log_util_function,set_rate,realtime_user1,absolute_sigma=True)
-ans, cov = fit
+# p0 = [1,1]
+# fit = curve_fit(norm_log_util_function,set_rate,p0,realtime_user1,absolute_sigma=True)
+# ans, cov = fit
 
-k_fitting = ans
-plt.xlabel('rates')
-plt.ylabel('Utilization(ri)')
-plt.title('Curve Fitting Graph')
-plt.plot(set_rate,realtime_user1,label='Sig')
-plt.plot(set_rate,norm_log_util_function(set_rate,k_fitting),label='Curve fit Sig')
-plt.legend()
-plt.show()
+# k_fitting = ans 
+# plt.xlabel('rates')
+# plt.ylabel('Utilization(ri)')
+# plt.title('Curve Fitting Graph')
+# plt.plot(set_rate, realtime_user1,label='Sig')
+# plt.plot(set_rate,norm_log_util_function(k_fitting),label='Curve fit Sig')
+# plt.legend()
+# plt.show()
 
